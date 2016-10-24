4d4873868a21c5be77d4f8a5bdf8fa3c81ae780d
joseeste92 <jose.esteche.92@gmail.com>
2016-10-24 20:51:09 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 var React = require('react');
 
-var NavBar = require('./NavBar.js');
+var NavBar = require('./NavBar');
+var Cover = require('./Cover');
+var Text = require('./Text');
 
 var App = React.createClass({
   render: function() {
     return (
       <div className="App">
         <NavBar />
+        <Cover />
+        <Text />
       </div>
     );
   }
